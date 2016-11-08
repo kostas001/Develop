5809bc05e16f9cfe05463d68c8532e673041c0b5
VarvitsK <VarvitsK@GRCORNB1027.eu.bm.net>
2016-11-08 15:33:49 +0200

    Add Console App at master branch

---
+++ Console_App/Console_App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is a console app developed by kostas");
+        }
+    }
+}
